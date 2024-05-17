8212d2884a71c30dfbf170fbd63bcf27d1a2997c
voyageth <voyageth@gmail.com>
2024-05-17 22:30:13 +0900

    :sparkles: player (#2)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 inputVector;
+    public float speed;
+
+    Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // add force
+        //rigidbody2D.AddForce(inputVector);
+        // velocity
+        //rigidbody2D.velocity = inputVector;
+        // move
+        Vector2 nextVector = inputVector.normalized * speed * Time.fixedDeltaTime;
+        rigidbody2D.MovePosition(rigidbody2D.position + nextVector);
     }
 }
