8a250aae10ead7d6d96429e9e04e018fd08e1e34
voyageth <voyageth@gmail.com>
2024-05-28 23:22:36 +0900

    캐릭터 해금🔓시스템 [유니티 뱀서라이크 14+]

---
+++ Assets/Scripts/AchiveManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchiveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
+
+    enum Achive
+    {
+        UnlockPotato,
+        UnlockBean,
+    }
+    Achive[] achives;
+    WaitForSecondsRealtime waitForSecondsRealtime;
+
+    private void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+        waitForSecondsRealtime = new WaitForSecondsRealtime(5);
+
+        if (!PlayerPrefs.HasKey("MyData"))
+            Init();
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+        
+        foreach(Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockCharacter();
+    }
+
+    void UnlockCharacter()
+    {
+        for (int index = 0;  index < lockCharacter.Length; index++)
+        {
+            string achiveName = achives[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            lockCharacter[index].SetActive(!isUnlock);
+            unlockCharacter[index].SetActive(isUnlock);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach (var achievement in achives)
+        {
+            CheckAchive(achievement);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTimeInSecond == GameManager.instance.maxGameTimeInSeccond;
+                break;
+        }
+
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for (int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return waitForSecondsRealtime;
+
+        uiNotice.SetActive(false);
+    }
+}
